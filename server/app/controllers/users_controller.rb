225d5081f4c97fca7a0b7e20268755f3f241ba15
Ayush Das <ayushdas221202@gmail.com>
2024-05-20 18:04:00 +0530

    server changes

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -3,26 +3,39 @@ class UsersController < ApplicationController
 
   def signup
     user = User.new(user_params)
+    puts "Received signup request with parameters: #{user_params.inspect}"
+
     if user.save
-      render json: { message: 'User created successfully' }
+      jwt_token = JWT.encode({ user_id: user.id }, Rails.application.credentials.dig(:jwt, :secret_key))
+      response = { success: true, token: jwt_token, name: user.name, email: user.email }
+      puts "Sending signup response: #{response.inspect}"
+      render json: response, status: :created
     else
-      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+      response = { success: false, error: user.errors.full_messages }
+      puts "Sending signup error response: #{response.inspect}"
+      render json: response, status: :unprocessable_entity
     end
   end
 
   def login
     user = User.find_by(email: params[:email])
+    puts "Received login request with parameters: #{params.inspect}"
+
     if user&.authenticate(params[:password])
       jwt_token = JWT.encode({ user_id: user.id }, Rails.application.credentials.dig(:jwt, :secret_key))
-      render json: { token: jwt_token }
+      response = { success: true, token: jwt_token, name: user.name, email: user.email }
+      puts "Sending login response: #{response.inspect}"
+      render json: response
     else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
+      response = { success: false, error: 'Invalid email or password' }
+      puts "Sending login error response: #{response.inspect}"
+      render json: response, status: :unauthorized
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_12_171703) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_19_150531) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
